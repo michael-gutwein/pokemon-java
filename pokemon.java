f0b423650c40a4a1b0bb71430e718ad33aae5c12
Michael Gutwein <114095949+michael-gutwein@users.noreply.github.com>
2023-01-11 13:23:33 -0500

    the 11th

--- pokemon.java
+++ pokemon.java
@@ -3,30 +3,91 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-class pokemon{
-
+class Pokemon{
+  private String num;
+  private String name;
+  private String type;
+  private String hp;
+  private String ap;
+  private String dp;
+  private int power;
+  
+ public Pokemon(String num, String name, String type, String hp, String ap, String dp) {
+   
+  this.num = num;  
+  this.name = name;
+  this.type = type;
+  this.hp = hp;
+  this.ap = ap;
+  this.dp = dp;
+   
+ }
+    public String getNum() {
+  return num; 
+ }
+    public void setNum(String num) {
+        this.num = num;
+    }
+ 
+ public String getName() {
+  return name; 
+ }
+ 
+ public void setName(String name) {
+        this.name = name;
+    }
+ 
+  public String getType() {
+  return type; 
+ }
+  
+   public void setType(String type) {
+        this.type = type;
+    }
+  
+   public String getHealth() {
+  return hp; 
+ }
+   public void setHealth(String hp) {
+        this.hp = hp;
+    }
+   
+    public String getAttack() {
+  return ap; 
+ }
+     public void setAttack(String ap) {
+        this.ap = ap;
+    }
+    public String getDefence() {
+  return dp; 
+ }
+    public void setDefence(String dp) {
+        this.dp = dp;
+    }
+ 
  public static void main(String[] args) {
  try {
  File pokemon = new File("pokemon.csv");
- File moves = new File("moves.csv");
  Scanner ScanPokemon = new Scanner(pokemon);
- Scanner ScanMove = new Scanner(moves);
  while (ScanPokemon.hasNextLine()) {
  String lineP = ScanPokemon.nextLine();
- System.out.println(Arrays.toString(lineP.split(",")));
- }
-  while (ScanMove.hasNextLine()) {
- String lineM = ScanMove.nextLine();
- System.out.println((Arrays.toString(lineM.split(","))));
+ String[] pList = lineP.split(",");
+ System.out.println(pList);
+   Pokemon p =  new Pokemon(pList[0], pList[1], pList[2], pList[3], pList[4], pList[5]);{
+
+     
+}
  }
  ScanPokemon.close();
- ScanMove.close();
  }
- 
- 
  catch (FileNotFoundException e) {
  System.out.println("File does not exist.");
  }
+
+
+
+ 
+ 
  }
 }
 
