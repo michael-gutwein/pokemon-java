f26099ba86d17dfe72aa4dfab64f1fb3d56af04c
Michael Gutwein <114095949+michael-gutwein@users.noreply.github.com>
2023-01-23 13:26:41 -0500

    rgrg
    rgr

--- items.java
+++ items.java
@@ -1,8 +1,36 @@
-class bag{
+class items{
+  public int hp;
+  private boolean isKO;
+  private boolean isPoisoned;
+  private boolean isBurning;
+  private boolean isFrozen;
+  private boolean isAsleep;
+  private boolean isConfused;
+  private boolean isParalyzed;
+  
   String[] items = {"Antidote", "Coffee", "Aloe Vera", "Potion", "Hyper Potion", "Super Potion", "Hot Chocolate", "Therapy Session", "Persim Berry", "SCP-500"};
   
+   public int getHP() {
+  return hp; 
+ }
+    public void setHP(int hp) {
+        this.hp = hp;
+    }
   
   
   
   
-}
+  
+  
+  
+  
+  
+  public static void main(String[] args){ 
+
+    }
+    
+    
+    
+    
+  }
+  
