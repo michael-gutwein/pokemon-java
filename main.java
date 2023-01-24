b26fdf465cc3e8a628f668ff5e85fd817092d23c
Michael Gutwein <114095949+michael-gutwein@users.noreply.github.com>
2023-01-24 14:45:27 -0500

    g
    g

--- main.java
+++ main.java
@@ -1,12 +1,20 @@
-import java.util.Arrays;
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-
 class main{
-public static void main(String[] args) {
 
-  
- }
+    public static String getName(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to Pokemon! What is your trainer name?");
+        String TrainerName = "Trainer " + input.nextLine();
+       System.out.println(TrainerName);
+       System.out.println("Your name is " + TrainerName + "? That is a cool name!");
+        return TrainerName;
+    }
+    
+    
+    public static void main(String[] args) {
+    getName();
+      
+
+    }
 }
